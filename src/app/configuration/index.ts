01087906bf33559563dfb3b31bdcc5f434fbc828
Дмитрий Чехонадских <loseex@vk.com>
2024-06-28 11:53:51 +0500

    upd

--- src/app/configuration/index.ts
+++ src/app/configuration/index.ts
@@ -1,6 +1,6 @@
 export const __APPLICATION_CONFIG__ = {
   api: {
-    baseUrl: "http://10.4.3.5:8000/api/v1",
+    baseUrl: "http://10.3.16.114:8000/api/v1",
     baseUrl_validate_auth_token: "http://10.3.11.193:8080/api",
   },
 };
